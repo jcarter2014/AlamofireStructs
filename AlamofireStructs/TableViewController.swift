3c73bf7b69b5f1f424ef4f9a92fc18bc854b33f3
Jack Carter <jcarter2014@gmail.com>
2018-10-01 21:27:10 -0400

    update to swift 4 decodable, added detail text label colors

--- AlamofireStructs/TableViewController.swift
+++ AlamofireStructs/TableViewController.swift
@@ -11,12 +11,16 @@ import Alamofire
 
 class TableViewController: UITableViewController, UISearchBarDelegate {
     
-    struct Coin {
+    struct Coin: Decodable {
         let name: String?
-        let price: String?
-        let percentChange1: String?
-        let percentChange24: String?
-        let percentChange7: String?
+        let price_usd: String?
+        let percent_change_1h: String?
+        let percent_change_24h: String?
+        let percent_change_7d: String?
+    }
+    
+    struct Coins: Decodable {
+        let coins: [Coin]
     }
     @IBOutlet weak var searchBar: UISearchBar!
     
@@ -30,30 +34,51 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func getPrices() {
-        Alamofire.request(url).responseJSON { (response) in
+        guard let url = URL(string: url) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
             
-            if let JSON = response.result.value as? [[String: Any]] {
-
-                for eachItem in JSON {
-                    
-                    let currentCoin = Coin(name: eachItem["name"] as? String, price: eachItem["price_usd"] as? String, percentChange1: eachItem["percent_change_1h"] as? String, percentChange24: eachItem["percent_change_24h"] as? String, percentChange7: eachItem["percent_change_7d"] as? String)
-                    self.coins.append(currentCoin)
+            do {
+                let coinList = try
+                    JSONDecoder().decode(Array<Coin>.self, from: data)
+                for coin in coinList {
+                    self.coins.append(coin)
                 }
+                
+                //print(coinList)
+                
+
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
             }
-            
-            self.tableView.reloadData()
-            self.searchCoins = self.coins
-            print(self.coins)
-        }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.searchCoins = self.coins
+            }
+            }.resume()
     }
+    
+//    func getPrices() {
+//        Alamofire.request(url).responseJSON { (response) in
+//
+//            if let JSON = response.result.value as? [[String: Any]] {
+//
+//                for eachItem in JSON {
+//
+//                    let currentCoin = Coin(name: eachItem["name"] as? String, price: eachItem["price_usd"] as? String, percentChange1: eachItem["percent_change_1h"] as? String, percentChange24: eachItem["percent_change_24h"] as? String, percentChange7: eachItem["percent_change_7d"] as? String)
+//                    self.coins.append(currentCoin)
+//                }
+//            }
+//
+//            self.tableView.reloadData()
+//            self.searchCoins = self.coins
+//            print(self.coins)
+//        }
+//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return coins.count
     }
-    
-//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "masterToDetail", sender: self)
-//    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -63,7 +88,25 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
         
         cell?.textLabel?.text = coins[indexPath.row].name
+        //cell?.detailTextLabel?.text = coins[indexPath.row].percent_change_7d
+        let weekChange = coins[indexPath.row].percent_change_7d
+        if let weekChangeAsDouble = (weekChange as NSString?)?.doubleValue {
+            cell?.detailTextLabel?.text = "\(weekChangeAsDouble)"
+            if weekChangeAsDouble > 0.0 {
+                cell?.detailTextLabel?.textColor = UIColor.green
+            } else {
+                cell?.detailTextLabel?.textColor = UIColor.red
+            }
+        }
         
+//        if let weekChangeToInt = Int(weekChange!) {
+//            print("success")
+//            let weekChangeToDouble = Double(weekChangeToInt)
+//            cell?.detailTextLabel?.text = "\(weekChangeToDouble)"
+//            cell?.detailTextLabel?.textColor = UIColor.green
+//        }
+        
+
         return cell!
     }
     
@@ -75,16 +118,16 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
                     if let name = coins[indexPath.row].name {
                         detailVC.name = name
                     }
-                    if let price = coins[indexPath.row].price {
+                    if let price = coins[indexPath.row].price_usd {
                         detailVC.price = price
                     }
-                    if let percentChange1 = coins[indexPath.row].percentChange1 {
+                    if let percentChange1 = coins[indexPath.row].percent_change_1h {
                         detailVC.percentChange1 = percentChange1
                     }
-                    if let percentChange24 = coins[indexPath.row].percentChange24 {
+                    if let percentChange24 = coins[indexPath.row].percent_change_24h {
                         detailVC.percentChange24 = percentChange24
                     }
-                    if let percentChange7 = coins[indexPath.row].percentChange7 {
+                    if let percentChange7 = coins[indexPath.row].percent_change_7d {
                         detailVC.percentChange7 = percentChange7
                     }
                 }
@@ -99,30 +142,18 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
             tableView.reloadData()
             return
         }
-        //searchCoins = coins
+        
         coins = searchCoins.filter { coin -> Bool in
-            //guard let text = searchBar.text else { return false }
+            
             (coin.name?.lowercased().contains(searchText.lowercased()))!
         }
         tableView.reloadData()
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
-        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         getPrices()
         searchBar.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ AlamofireStructs/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  AlamofireStructs
+//
+//  Created by John Carter on 10/1/18.
+//  Copyright © 2018 Jack Carter. All rights reserved.
+//
+
+import Foundation
+
+struct Test {
+    let test: Int
+}
